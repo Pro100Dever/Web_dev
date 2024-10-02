18ae4f3c0f8439ee4f618e72e0c0059e8519fd00
Volodymyr Berestenko <koltak2mail.ru@gmail.com>
2024-10-02 12:19:55 +0200

    1

---
+++ JS/Class_Work/Lesson1/script.js
@@ -0,0 +1,46 @@
+/* 
+    1.String 'Hello world'
+    2.Number 44
+    3. boolean true || false
+    4. Object = {}
+    5.Symbol
+    6.Bigint
+    7.Null
+    8.Undefined
+*/
+
+/* 
+let a;
+let b = null;
+let myName = "Ich heisse Volodymyr!";
+let city = "Ich bin in der Ukraine geboren.";
+console.log(typeof a);
+console.log(typeof b);
+console.log(myName, typeof myName);
+console.log(city, typeof city);
+ */
+
+/* const userName = 'Volodymyr'
+const firstNumber = 1
+const secondNumber = 5 */
+/* const sum = firstNumber + secondNumber */
+
+/* if (counter < 5) {
+    counter++
+} */
+
+/* console.log(sum); */
+/* console.log(firstNumber + secondNumber); */
+
+
+// конкатинация:
+const userName = 'Volodymyr'
+const firstNumber = 1
+const secondNumber = 5
+
+const sum = "Hello, my name is" + userName + 'my age is ' + secondNumber + ' etwas';
+console.log(sum)
+
+// интерпаляция
+const sum1 = `Hello, my name is ${userName} my age is ${secondNumber} etwas`;
+console.log(sum1)
