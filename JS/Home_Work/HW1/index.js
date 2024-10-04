21c0dd523e76e7c4875d6f1ddaf17a828eb90f2b
Volodymyr Berestenko <koltak2mail.ru@gmail.com>
2024-10-04 08:34:30 +0200

    1

---
+++ JS/Home_Work/HW1/index.js
@@ -0,0 +1,9 @@
+const firstName = 'Volodymyr'
+const lastName = 'Berestenko'
+const greeting = `Hello, ${firstName} ${lastName}`
+console.log(greeting)
+// =================================================
+const sentencePart1 = 'I like'
+const sentencePart2 = 'programmiring'
+const completeSentence = sentencePart1 + ' ' + sentencePart2
+console.log(completeSentence)
