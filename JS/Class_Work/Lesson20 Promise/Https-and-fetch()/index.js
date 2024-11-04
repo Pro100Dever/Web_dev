27926dd8df3a9fcb1861309400389e63b6c6bda3
Volodymyr Berestenko <koltak2mail.ru@gmail.com>
2024-11-04 12:17:19 +0100

    fetch lernen

---
+++ JS/Class_Work/Lesson20 Promise/Https-and-fetch()/index.js
@@ -0,0 +1,36 @@
+/* const p1 = new Promise(res => res('hello world'))
+console.log(p1)
+
+p1.then(res => console.log(res))
+//============================
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(res => res.json())
+  .then(data => console.log(data))
+
+async function fetchFn() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+  const data = await res.json()
+  console.log(data)
+}
+fetchFn()
+//================================
+fetch('https://jsonplaceholder.typicode.com/todos', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    title: 'HELLO VLADIMIR',
+    completed: true,
+  }),
+})
+  .then(res => res.json())
+  .then(res => console.log(res))
+ */
+
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//   .then(res => res.json())
+//   .then(data => console.log(data.map(item => item.id)))
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(res => res.json())
+  .then(data => {
+    return data.map(item => console.log(item.id))
+  })
