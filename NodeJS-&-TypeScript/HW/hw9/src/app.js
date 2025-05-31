0f91dee81e5225c122349e020501181c38d4774e
Volodymyr Berestenko <koltak2mail.ru@gmail.com>
2025-05-31 19:24:53 +0200

    hw9

--- NodeJS-&-TypeScript/HW/hw9/src/app.js
+++ NodeJS-&-TypeScript/HW/hw9/src/app.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt'
 import 'dotenv/config.js'
 import express from 'express'
+import jwt from 'jsonwebtoken'
 import sequelize from './config/db.js'
 import {
   checkAuth,
@@ -34,104 +35,163 @@ app.post('/register', async (req, res) => {
       .json({ error: 'User with this email already exists' })
   }
   const hashedPassword = await bcrypt.hash(password, 10)
-  const User = await User.create({
+  const newUser = await User.create({
     email,
     password: hashedPassword,
     mustChangePassword: mustChangePassword || false,
     role: role || 'user',
   })
-  res.status(201).json(User)
+  res.status(201).json(newUser)
 })
 
-app.post('/change-password', checkMustChangePassword, async (req, res) => {
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Email и пароль обязательны' })
+  }
+
   try {
-    const { newPassword } = req.body
-    if (!newPassword || newPassword.length < 6) {
-      return res
-        .status(400)
-        .json({ error: 'Пароль должен быть не короче 6 символов' })
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      return res.status(404).json({ error: 'Пользователь не найден' })
     }
 
-    const hashedPassword = await bcrypt.hash(newPassword, 10)
-    const user = await User.findByPk(req.user.id)
-    if (!user) return res.status(404).json({ error: 'Пользователь не найден' })
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: 'Неверный пароль' })
+    }
 
-    user.password = hashedPassword
-    user.mustChangePassword = false
-    await user.save()
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+        mustChangePassword: user.mustChangePassword,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    )
 
-    res.json({ message: 'Пароль успешно обновлён' })
+    res.json({ message: 'Успешный вход', token })
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: 'Ошибка сервера' })
   }
 })
 
-app.delete('/delete-account', checkAuth, async (req, res) => {
-  try {
-    const id = req.user.id
-    const curPass = req.body.password
+app.post(
+  '/change-password',
+  checkAuth,
+  checkMustChangePassword,
+  async (req, res) => {
+    try {
+      const { newPassword } = req.body
+      if (!newPassword || newPassword.length < 6) {
+        return res
+          .status(400)
+          .json({ error: 'Пароль должен быть не короче 6 символов' })
+      }
 
-    if (!curPass) {
-      return res.status(400).json({ error: 'Пароль обязателен' })
+      const hashedPassword = await bcrypt.hash(newPassword, 10)
+      const user = await User.findByPk(req.user.id)
+      if (!user)
+        return res.status(404).json({ error: 'Пользователь не найден' })
+
+      user.password = hashedPassword
+      user.mustChangePassword = false
+      await user.save()
+
+      res.json({ message: 'Пароль успешно обновлён' })
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ error: 'Ошибка сервера' })
     }
-
-    const user = await User.findByPk(id)
-
-    if (!user) {
-      return res.status(404).json({ error: 'Пользователь не найден' })
-    }
-
-    const checkPass = await bcrypt.compare(curPass, user.password)
-    if (!checkPass) {
-      return res.status(400).json({ error: 'Неверный пароль' })
-    }
-
-    await user.destroy()
-    res.json({ message: 'Аккаунт удалён' })
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({ error: 'Ошибка сервера' })
   }
-})
+)
 
-app.get('/admin', adminOnly, async (req, res) => {
-  res.json('Admin only Page')
-})
+app.delete(
+  '/delete-account',
+  checkAuth,
+  checkMustChangePassword,
+  async (req, res) => {
+    try {
+      const id = req.user.id
+      const curPass = req.body.password
 
-app.post('/change-email', checkAuth, async (req, res) => {
-  try {
-    const { email: newEmail, password: curPassword } = req.body
-    if (!newEmail || !curPassword || curPassword.length < 6) {
-      return res
-        .status(400)
-        .json({ error: 'Нужны все данные и пароль минимум 6 символов' })
-    }
-    const user = await User.findByPk(req.user.id)
-    if (!user) {
-      return res.status(404).json({ error: 'Пользователь не найден' })
-    }
-    // Проверяем текущий пароль
-    const isPasswordCorrect = await bcrypt.compare(curPassword, user.password)
-    if (!isPasswordCorrect) {
-      return res.status(400).json({ error: 'Неверный пароль' })
-    }
-    // Проверяем, что новый email уникален
-    const emailExists = await User.findOne({ where: { email: newEmail } })
-    if (emailExists) {
-      return res.status(400).json({ error: 'Этот email уже занят' })
-    }
+      if (!curPass) {
+        return res.status(400).json({ error: 'Пароль обязателен' })
+      }
 
-    // Обновляем email и сохраняем пользователя
-    user.email = newEmail
-    await user.save()
+      const user = await User.findByPk(id)
 
-    res.json({ message: 'Email успешно обновлён' })
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ error: 'Ошибка сервера' })
+      if (!user) {
+        return res.status(404).json({ error: 'Пользователь не найден' })
+      }
+
+      const checkPass = await bcrypt.compare(curPass, user.password)
+      if (!checkPass) {
+        return res.status(400).json({ error: 'Неверный пароль' })
+      }
+
+      await user.destroy()
+      res.json({ message: 'Аккаунт удалён' })
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({ error: 'Ошибка сервера' })
+    }
   }
-})
+)
+
+app.get(
+  '/admin',
+  checkAuth,
+  checkMustChangePassword,
+  adminOnly,
+  async (req, res) => {
+    res.json('Admin only Page')
+  }
+)
+
+app.post(
+  '/change-email',
+  checkAuth,
+  checkMustChangePassword,
+  async (req, res) => {
+    try {
+      const { email: newEmail, password: curPassword } = req.body
+      if (!newEmail || !curPassword || curPassword.length < 6) {
+        return res
+          .status(400)
+          .json({ error: 'Нужны все данные и пароль минимум 6 символов' })
+      }
+      const user = await User.findByPk(req.user.id)
+      if (!user) {
+        return res.status(404).json({ error: 'Пользователь не найден' })
+      }
+      // Проверяем текущий пароль
+      const isPasswordCorrect = await bcrypt.compare(curPassword, user.password)
+      if (!isPasswordCorrect) {
+        return res.status(400).json({ error: 'Неверный пароль' })
+      }
+      // Проверяем, что новый email уникален
+      const emailExists = await User.findOne({ where: { email: newEmail } })
+      if (emailExists) {
+        return res.status(400).json({ error: 'Этот email уже занят' })
+      }
+
+      // Обновляем email и сохраняем пользователя
+      user.email = newEmail
+      await user.save()
+
+      res.json({ message: 'Email успешно обновлён' })
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ error: 'Ошибка сервера' })
+    }
+  }
+)
 
 app.listen(PORT, async () => {
   try {
--- NodeJS-&-TypeScript/HW/hw9/src/middleware/authMiddleware.js
+++ NodeJS-&-TypeScript/HW/hw9/src/middleware/authMiddleware.js
@@ -1,15 +1,27 @@
+import jwt from 'jsonwebtoken'
+
 function checkMustChangePassword(req, res, next) {
   // Предполагаем, что пользователь уже аутентифицирован и доступен в req.user
-  if (req.user && req.user.mustChangePassword === true) {
+  if (
+    req.user &&
+    req.user.mustChangePassword === true &&
+    req.path !== '/change-password'
+  ) {
     return res.redirect('/change-password') // или другой путь смены пароля
   }
   next()
 }
 
 function checkAuth(req, res, next) {
-  if (!req.user) {
-    return res.redirect('/register')
+  const token = req.headers.authorization?.split(' ')[1]
+  if (!token) return res.status(401).json({ error: 'Токен отсутствует' })
+
+  try {
+    const userData = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = userData // должен содержать id, role, mustChangePassword и т.п.
+    next()
+  } catch (err) {
+    return res.status(401).json({ error: 'Неверный токен' })
   }
-  next()
 }
 export { checkAuth, checkMustChangePassword }
